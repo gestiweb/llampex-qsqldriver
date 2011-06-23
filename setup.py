810ae3bd0bbefb0acb3d1941dd06a262e650ec56
Àngel Fernández <angel@calidae.net>
2011-06-23 11:24:53 +0200

    Prepared to use distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+from distutils.core import setup
+setup(
+    name = 'qsqlrpcdriver',
+    version = '0.1',
+    description = 'Driver for QtSQL, using server-client method with bjsonrpc',
+    author = 'Àngel Fernández Ibáñez (Calidae)',
+    author_email = 'angel@calidae.net',
+    packages = ['qsqlrpcdriver']
+)
